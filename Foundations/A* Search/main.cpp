cadc26621d4402bf47b322978b5f33c556d6e74c
Nate Pierce <nate.pierce@maymobility.com>
2022-05-05 14:52:01 -0400

    Started A* section

---
+++ Foundations/A* Search/main.cpp
@@ -0,0 +1,80 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+using std::abs;
+
+enum class State {kEmpty, kObstacle};
+
+vector<vector<State>> Search(vector<vector<State>> board, vector<int> start, vector<int> goal) {
+  vector<vector<State>> minPath;
+  cout << "No Path found!";
+  return minPath;
+}
+
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+      if (n == 0) {
+        row.push_back(State::kEmpty);
+      } else {
+        row.push_back(State::kObstacle);
+      }
+    }
+    return row;
+}
+
+
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile (path);
+  vector<vector<State>> board{};
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
+}
+
+// TODO: Write the Search function stub here.
+
+
+string CellString(State cell) {
+  switch(cell) {
+    case State::kObstacle: return "⛰️   ";
+    default: return "0   "; 
+  }
+}
+
+
+void PrintBoard(const vector<vector<State>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      cout << CellString(board[i][j]);
+    }
+    cout << "\n";
+  }
+}
+
+
+int main() {
+  // TODO: Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
+  vector<int> init{0,0};
+  vector<int> goal{4,5};
+  auto board = ReadBoardFile("1.board");
+  // TODO: Call Search with "board", "init", and "goal". Store the results in the variable "solution".
+  vector<vector<State>> solution = Search(board, init, goal);
+  // TODO: Change the following line to pass "solution" to PrintBoard.
+  PrintBoard(solution);
+}
